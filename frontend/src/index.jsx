b7d1d90230d99a091a3e9d4b0cddc8d1841036f5
Rostyslav <rostislav.levitskiy.05@gmail.com>
2025-02-12 10:08:55 +0100

    redux initialized

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
+import { Provider } from 'react-redux'
+import { store } from "./state/store.js";
 import "./scss/style.scss"
 
 const root = document.querySelector("#root")
-	? document.querySelector("#root")
-	: document.querySelector(".wrapper");
+    ? document.querySelector("#root")
+    : document.querySelector(".wrapper");
 
 ReactDOM.createRoot(root).render(
-	<App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
