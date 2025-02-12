e698439b6960789cff8f39b558914e6f7632d9a8
Rostyslav <rostislav.levitskiy.05@gmail.com>
2025-02-12 10:14:39 +0100

    userdata is saved by login

--- frontend/src/functions/useToken.jsx
+++ frontend/src/functions/useToken.jsx
@@ -1,40 +1,55 @@
 import { useState } from 'react';
+import { jwtDecode } from "jwt-decode";
+import { useDispatch } from 'react-redux';
+import { initUser, logoutUser } from '../state/user/userSlice';
 
 function useToken() {
+    const dispatch = useDispatch();
+    const [token, setToken] = useState(getToken());
 
-  function getToken() {
-    const name = 'token=';
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-    for (let i = 0; i < cookieArray.length; i++) {
-      let cookie = cookieArray[i].trim();
-      if (cookie.indexOf(name) === 0) {
-        return cookie.substring(name.length, cookie.length);
-      }
+    function getToken() {
+        const name = 'token=';
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i].trim();
+            if (cookie.indexOf(name) === 0) {
+                const token = cookie.substring(name.length, cookie.length);
+                const decodedToken = jwtDecode(token);
+                dispatch(initUser({
+                    username: decodedToken.sub.username,
+                    admin: decodedToken.sub.admin
+                }))
+                return token
+            }
+        }
+        return null;
     }
-    return null;
-  }
 
-  const [token, setToken] = useState(getToken());
+    function saveToken(userToken) {
+        const d = new Date();
+        d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000)); // Set cookie to expire in 7 days
+        const expires = "expires=" + d.toUTCString();
+        const decodedToken = jwtDecode(userToken);
+        dispatch(initUser({
+            username: decodedToken.sub.username,
+            admin: decodedToken.sub.admin
+        }))
+        document.cookie = `token=${userToken};${expires};path=/`;
+        setToken(userToken);
+    }
 
-  function saveToken(userToken) {
-    const d = new Date();
-    d.setTime(d.getTime() + (7*24*60*60*1000)); // Set cookie to expire in 7 days
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = `token=${userToken};${expires};path=/`;
-    setToken(userToken);
-  }
+    function removeToken() {
+        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        dispatch(logoutUser());
+        setToken(null);
+    }
 
-  function removeToken() {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    setToken(null);
-  }
-
-  return {
-    setToken: saveToken,
-    token,
-    removeToken
-  }
+    return {
+        setToken: saveToken,
+        token,
+        removeToken
+    }
 
 }
 
